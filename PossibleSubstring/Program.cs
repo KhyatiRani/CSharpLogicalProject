86e24ec0fca8fc16493eec44a2c170e7353c7000
khyati <khyati.rani@cengage.com>
2024-05-28 21:56:55 +0530

    PossibleSubstring

---
+++ PossibleSubstring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace LogicalPrograms
+{
+    class PossibleSubstring
+    {
+        static void Main(string[] args)
+        {
+           
+            string inputString = "hall";
+            Console.Write("All possible substring are :");
+            for (int i = 0; i < inputString.Length; ++i)
+            {
+                StringBuilder subString = new StringBuilder(inputString.Length - i);
+                for (int j = i; j < inputString.Length; ++j)
+                {
+                    
+                    subString.Append(inputString[j]);
+                    Console.Write(subString + " ");
+                }
+            }
+        }
+    }
+}
+        
